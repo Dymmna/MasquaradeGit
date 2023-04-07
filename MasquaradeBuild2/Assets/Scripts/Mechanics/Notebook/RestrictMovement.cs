3eeb0138fa48425d9e2484b712e6af6e49813bd7
yshel_v_les <nikakate2@gmail.com>
2023-04-07 16:52:47 +0300

    Clamp for Clues

---
+++ MasquaradeBuild2/Assets/Scripts/Mechanics/Notebook/RestrictMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestrictMovement : MonoBehaviour
+{
+    private Transform objTransform;
+
+    float speed = 0;
+
+
+   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+    void Update()
+    {
+      
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -1.94f, 2.02f), Mathf.Clamp(transform.position.y, -3.3f, 3.44f), transform.position.z);
+
+    }
+
+}
