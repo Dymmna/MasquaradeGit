f0f536cad38ffbf06467374793ea271d9e98aa31
yshel_v_les <nikakate2@gmail.com>
2023-04-14 17:21:32 +0300

    Puzzle Gavkoshmig

---
+++ MasquaradeBuild2/Assets/attatchDetail.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class attatchDetail : MonoBehaviour
+{
+    private Transform detailPosition;
+
+    private Vector2 initialPosition;
+
+    private float deltaX, deltaY;
+
+    public static bool locked;
+
+    public Color activeColor;
+    public static Color _activeColor;
+
+    public string NewLevel = "Place name here";
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _activeColor = activeColor;
+    }
+    void Start()
+    {       
+        StartCoroutine(DetailContr());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    IEnumerator DetailContr()
+    {
+        GameObject[] details = GameObject.FindGameObjectsWithTag("detail");
+        while (true)
+        {
+            bool allInPlace = true;
+            foreach (GameObject detail in details)
+            {
+                if (!detail.GetComponent<placePosition>().placeMe)
+                {
+                    allInPlace = false;
+                    break;
+                }
+            }
+            if (allInPlace)
+            {
+                
+                Debug.Log("VSE SOBRALY");
+                StopAllCoroutines();
+                //сюда смена сцены
+
+            }
+            yield return new WaitForSeconds(1f);
+        } 
+    }
+}
---
+++ MasquaradeBuild2/Assets/placePosition.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class placePosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform detailPosition;
+    public bool placeMe = false;
+
+    private Color activeColor = new Color(0.3f, 0, 0);
+    private Vector2 offset;
+    private Vector3 position;
+
+    private SpriteRenderer sr;
+    private bool canDrop;
+
+    private bool drag;
+
+    //public AudioSource wood;
+    //public AudioSource woodDown;
+
+    private void Start()
+    {
+        activeColor = attatchDetail._activeColor;
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (drag && !canDrop && !placeMe) sr.color = activeColor;
+        else sr.color = Color.white;
+        
+    }
+
+    private void OnMouseDrag()
+    {
+        if (!placeMe)
+        {
+            position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            position.z = 0;
+            transform.position = position + (Vector3)offset;
+
+            float distance = Vector2.Distance(transform.position, detailPosition.position);
+            canDrop = distance < 1f;
+        }     
+
+    }
+
+    private void OnMouseDown()
+    {
+        //wood.Play();
+        position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        position.z = 0;
+        offset = transform.position - position;
+        drag = true;
+    }
+
+    private void OnMouseUp()
+    {
+        
+        drag = false;
+        if (canDrop && !placeMe)
+        {
+            transform.position = detailPosition.position;
+            placeMe = true;
+           // woodDown.Play();
+        }
+    }
+}
